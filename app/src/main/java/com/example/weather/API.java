71987fa867d545a598209f2d3701be52506ce113
cheng-zhi-max <2896786151@qq.com>
2022-07-04 10:22:11 +0800

    天气app 1.1 增加下拉刷新功能

--- app/src/main/java/com/example/weather/API.java
+++ app/src/main/java/com/example/weather/API.java
@@ -6,6 +6,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface API {
-    @GET("api?appid=91395745&appsecret=40OfE1a7&version=v61")
+    @GET("易客云天气自己申请账号，有免费天气api次数")
     Call<WeatherBean> weather();
 }
